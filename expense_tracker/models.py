343249442c89727dafc11bab3399cade3c0f7022
UzziKyle <uzzielkyleynciong0824@gmail.com>
2023-11-16 04:11:48 +0800

    add home page and expense form page

--- expense_tracker/models.py
+++ expense_tracker/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Expense(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    amount = models.DecimalField(
+        blank=False,
+        default=0,
+        decimal_places=2,
+        max_digits=10,
+    )
+    date = models.DateTimeField(default=timezone.now, blank=False)
+    
+    def __str__(self):
+        return str(self.amount)
+    
--- expense_tracker/views.py
+++ expense_tracker/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Expense
+from .forms import ExpenseForm
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Tagasubaybay ng Gastos')
+    return render(request, 'expense_tracker/home.html')
+
+
+def add_expense(request):
+    if request.method == 'GET':
+        context = {'form': ExpenseForm()}
+        return render(request, 'expense_tracker/expense_form.html', context)
+    
